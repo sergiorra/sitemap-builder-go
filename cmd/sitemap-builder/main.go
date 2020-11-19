df8c02bc1773706a5ed6f6dad0ed44d86179eb4d
Sergi Orra <sergiorra17@gmail.com>
2020-11-19 09:40:13 +0100

    working on builder

--- cmd/sitemap-builder/main.go
+++ cmd/sitemap-builder/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"os"
 
+	"github.com/sergiorra/sitemap-builder-go/internal/builder"
+)
+
+func main() {
+	urlFlag := flag.String("url", "https://gophercises.com", "The url that you want to build a sitemap for")
+	maxDepth := flag.Int("depth", 10, "The maximum number of links deep to traverse")
+	flag.Parse()
+
+	pages := builder.Bfs(*urlFlag, *maxDepth)
+	toXml := builder.Urlset{
+		Xmlns: builder.Xmlns,
+	}
+	for _, page := range pages {
+		toXml.Urls = append(toXml.Urls, builder.Loc{page})
+	}
+
+	fmt.Print(xml.Header)
+	enc := xml.NewEncoder(os.Stdout)
+	enc.Indent("", "  ")
+	if err := enc.Encode(toXml); err != nil {
+		panic(err)
+	}
+	fmt.Println()
 }
---
+++ internal/builder/builder.go
@@ -0,0 +1,105 @@
+package builder
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/sergiorra/sitemap-builder-go/internal/parser"
+)
+
+/*
+   1. GET the webpage
+   2. parse all the links on the page
+   3. build proper urls with our links
+   4. filter out any links w/ a diff domain
+   5. Find all pages (BFS)
+   6. print out XML
+*/
+
+const Xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9"
+
+type Loc struct {
+	Value string `xml:"loc"`
+}
+
+type Urlset struct {
+	Urls  []Loc  `xml:"url"`
+	Xmlns string `xml:"xmlns,attr"`
+}
+
+type empty struct{}
+
+func Bfs(urlStr string, maxDepth int) []string {
+	seen := make(map[string]empty)
+	var q map[string]empty
+	nq := map[string]empty{
+		urlStr: {},
+	}
+	for i := 0; i <= maxDepth; i++ {
+		q, nq = nq, make(map[string]empty)
+		if len(q) == 0 {
+			break
+		}
+		for url, _ := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = empty{}
+			for _, link := range get(url) {
+				nq[link] = empty{}
+			}
+		}
+	}
+	ret := make([]string, 0, len(seen))
+	for url, _ := range seen {
+		ret = append(ret, url)
+	}
+	return ret
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
+	if err != nil {
+		return []string{}
+	}
+	defer resp.Body.Close()
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+}
+
+func hrefs(r io.Reader, base string) []string {
+	links, _ := parser.Parse(r)
+	var ret []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			ret = append(ret, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			ret = append(ret, l.Href)
+		}
+	}
+	return ret
+}
+
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, link := range links {
+		if keepFn(link) {
+			ret = append(ret, link)
+		}
+	}
+	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
+	}
+}
